fba4599f20fc69f3d93f73dd66d78c7c607d9e9c
kuhnxia <80392261+kuhnxia@users.noreply.github.com>
2021-06-13 21:40:47 -0300

    Create Lc20.java

---
+++ Code/src/Lc20.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Lc20 {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '('){
+                stk.push(')');
+            }
+            else if(c == '{'){
+                stk.push('}');
+            }
+            else if(c == '['){
+                stk.push(']');
+            }
+            else if(stk.isEmpty() || stk.pop() != c){
+                return false;
+            }
+        }
+        return stk.isEmpty();
+
+    }
+}
