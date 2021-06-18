0dad222010d72a74e2247959d41164c6480f9749
kuhnxia <80392261+kuhnxia@users.noreply.github.com>
2021-06-17 23:15:25 -0300

    Create Lc34.java

---
+++ Code/src/Lc34.java
@@ -0,0 +1,13 @@
+public class Lc34 {
+    public int searchInsert(int[] nums, int target) {
+        int index = nums.length;
+        for(int i=0; i<nums.length; i++){
+            if(target <= nums[i]){
+                index = i;
+                break;
+            }
+        }
+        return index;
+
+    }
+}
