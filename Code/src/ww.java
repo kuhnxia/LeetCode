caf35f2c1e70a441aa98ea36fb4b25462cec3800
kuhnxia <80392261+kuhnxia@users.noreply.github.com>
2021-07-23 00:48:26 -0300

    test

---
+++ Code/src/ww.java
@@ -0,0 +1,14 @@
+public class ww {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (j == 100)
+                    break;
+                ;
+            }
+            a++;
+        }
+        System.out.println(a);
+    }
+}
