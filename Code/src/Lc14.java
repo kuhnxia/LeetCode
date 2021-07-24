1b453c6135a1e86347e65390491062ccc6eb8548
kuhnxia <80392261+kuhnxia@users.noreply.github.com>
2021-07-24 20:26:58 -0300

    Update Lc14.java

--- Code/src/Lc14.java
+++ Code/src/Lc14.java
@@ -1,23 +1,13 @@
 public class Lc14 {
     public String longestCommonPrefix(String[] strs) {
-        String lcp = "";
-        if(strs.length == 1){
-            lcp = strs[0];
-        }
-        else{
-            for(int i=0; i<200; i++){
-                for(int j=0; j<strs.length; j++){
-                    if(i>=strs[j].length()){
-                        i = 200;
-                    }
-                    else if(strs[j].charAt(i) - strs[0].charAt(i) != 0){
-                        i = 200;
-                    }
-                }
-                if(i<200)
-                    lcp += strs[0].charAt(i);
+        if(strs == null || strs.length == 0)
+            return "";
+        for(int i=0; i< strs[0].length(); i++){
+            for(int j=0; j<strs.length; j++){
+                if(i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i))
+                    return strs[0].substring(0,i);
             }
         }
-        return lcp;
+        return strs[0];
     }
 }
